79d70c1b08e9f5140b610b90f238d3c878c4b27e
Vihan Perera <vihanpereraux@gmail.com>
2023-07-22 20:42:54 +0530

    Update server script

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
+// var express = require('express');
+// var cors = require('cors');
+// var app = express();
+
+// app.use(cors({
+//     origin: "*"
+// }))
+
+
+// var server = app.listen(3000);
+// app.use(express.static('public'));
+
+// console.log("socket server is running");
+// var socket = require('socket.io');
+// var io = socket(server);
+
+// // event
+// io.sockets.on('connection', function(socket){
+//     socket.on('mouse', function(clientData){
+//         socket.broadcast.emit('mouse', clientData);
+//         console.log(clientData);
+//     });
+//     console.log('new connection ' + socket.id);
+// });
+
+
 var express = require('express');
-var cors = require('cors');
-
 var app = express();
-var server = app.listen(3000);
-app.use(cors({
-    origin: 'https://air-ink-client-01.netlify.app/'
-}));
-app.use(express.static('public'));
+var http = require('http').createServer(app);
+var socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "*"
+    }
+});
 
-console.log("socket server is running");
-var socket = require('socket.io');
-var io = socket(server);
-
-// event
-io.sockets.on('connection', function(socket){
-    socket.on('mouse', function(clientData){
-        socket.broadcast.emit('mouse', clientData);
-        console.log(clientData);
+http.listen(3000, function(){
+    console.log("socket server is running");
+    socketIO.on("connection", function(socket){
+        console.log('new connection ' + socket.id);
     });
-    console.log('new connection ' + socket.id);
 });
